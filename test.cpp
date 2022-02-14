563b24adc88e84ccea7aa549873328a3ccfed339
leezhenglong-dev <84690187+leezhenglong-dev@users.noreply.github.com>
2022-02-14 12:21:18 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,8 +1,12 @@
 #include<iostream>
 using namespace std;
 
+
 int main()
-{
+{	
+	//show whatever contents between the double quotes 
+	//on the screen
+	
 	cout<<"Hello world"<<endl;
 	
 	return 0;
